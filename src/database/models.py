a7a136c13796b947cd626aa93dd742204b626332
Armando <armandobp765@gmail.com>
2022-12-05 10:25:50 -0600

    Added settings, models and create_sessionmaker

---
+++ src/database/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from src.database.settings import Base
+
+
+class Solution(Base):
+    __tablename__ = "solutions"
+    id =  Column(Integer, primary_key=True)
+
+
+class Position(Base):
+    __tablename__ = "positions"
+    id =  Column(Integer, primary_key=True)
+    x_position = Column(Integer)
+    y_position = Column(Integer)
+    solution_pk = Column(ForeignKey("solutions.id"))
---
+++ src/database/settings.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+DATABASE = {
+    "DB_HOST": os.getenv("DB_HOST", default="db"),
+    "DB_PORT": os.getenv("DB_PORT", default=5432),
+    "DB_PASSWORD": os.getenv("DB_PASSWORD", default="postgres"),
+    "DB_USERNAME": os.getenv("DB_USERNAME", default="postgres"),
+    "DB_NAME": os.getenv("DB_NAME", default="eight_queens")
+}
