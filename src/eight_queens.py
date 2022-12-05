0a6d3c9ac2c4f2f128e59cda9486c132255bcfd2
Armando <armandobp765@gmail.com>
2022-12-04 21:04:45 -0600

    Removed non-used variables

--- src/eight_queens.py
+++ src/eight_queens.py
@@ -18,22 +18,18 @@ def position_is_available(position: Dict, queens: List) -> bool:
 def place_queen(row, queens, n, solutions):
     if row == n:
         solutions.append(queens.copy())
-        return 1
-    else:
-        total_solutions = 0
 
+    else:
         for col in range(n):
             position = {"x": col, "y": row}
 
             if position_is_available(position, queens):
                 queens[row] = position
-                total_solutions += place_queen(row + 1, queens, n, solutions)
-
-        return total_solutions
+                place_queen(row + 1, queens, n, solutions)
 
 
 def get_solutions(n):
-    queens = [None for i in range(n)]
+    queens = [None] * 4
     solutions = []
     row = 0
     place_queen(row, queens, n, solutions)
